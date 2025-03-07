c6d3771b02c03bc80aa79fffab3d203cdd28a38e
Technology Gardening <33565151+TechnologyGardening@users.noreply.github.com>
2025-03-07 11:24:33 +0530

    new

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const module1 = require("./functions/primarytest");
 const app = express();
 const port = 4565;
+var cors = require("cors");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get("/db/user/insert", module1.FunctInsert);
 app.get("/db/user/update", module1.FunctUpdate);
